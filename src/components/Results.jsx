ff3d0df5b9f2fac987c67989880b0c59cc9ba24e
wassim-mouloud <simo.rko10@gmail.com>
2023-08-10 16:45:25 +0100

    fixing responsive bugs

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -13,7 +13,7 @@ function Results({firstP, secondP, gm, set1, set2, set3,set4,set5, points, sets,
   return (
     <>
     {gm===false?(
-         <motion.div className='first:ml-[20px] last:mr-[20px] md:hover:scale-[1.05] mb-[20px] transition-all relative bg-[#091327] md:w-[calc(100vw/4)] h-[200px]  w-[80vw] flex flex-col gap-4 p-5 rounded-[10px] mt-[20px] justify-center flex-shrink-0 cursor-grab'>
+         <motion.div className='first:ml-[20px] last:mr-[20px] md:hover:scale-[1.05] mb-[20px] transition-all relative bg-[#091327] w-[85vw] sm:w-[400px] md:w-[420px] lg:w-[420px] h-[200px]   flex flex-col gap-4 p-5 rounded-[10px] mt-[20px] justify-center flex-shrink-0 cursor-grab'>
             <div className='flex gap-2' >
                 <p className='text-[#20BF55] text-[12px]' >{tournament}</p>
                 <p className='text-[#20BF55] text-[12px]' >({type})</p>
